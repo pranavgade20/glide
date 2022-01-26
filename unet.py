a525a3d7f7ad715650b3e7bf5ddf0cafbb0bebe4
Pranav Gade <pranavgade20@gmail.com>
2022-01-26 12:21:50 -0800

    more code

--- unet.py
+++ unet.py
@@ -10,7 +10,7 @@ class ResidualBlock(nn.Module):
     """
     A residual block that can optionally change the number of channels.
 
-    :param channels: the number of input channels.
+    :param in_channels: the number of input channels.
     :param emb_channels: the number of timestep embedding channels.
     :param dropout: the rate of dropout.
     :param out_channels: if specified, the number of out channels.
@@ -21,7 +21,7 @@ class ResidualBlock(nn.Module):
     """
     def __init__(
         self,
-        channels,
+        in_channels,
         emb_channels,
         out_channels,
         use_conv=False,
@@ -30,20 +30,19 @@ class ResidualBlock(nn.Module):
     ):
         super().__init__()
         assert out_channels is None
-        self.channels = channels
+        self.in_channels = in_channels
         self.emb_channels = emb_channels
         self.dropout = dropout
-        self.out_channels = out_channels or channels
+        self.out_channels = out_channels or in_channels
         self.use_conv = use_conv
         self.use_scale_shift_norm = use_scale_shift_norm
 
         self.in_layers = nn.Sequential(
-            nn.GroupNorm(num_groups=32, num_channels=self.channels, eps=1e-5),
+            nn.GroupNorm(num_groups=32, num_channels=self.in_channels, eps=1e-5),
             nn.SiLU(),
-            nn.Conv2d(self.channels, self.out_channels, kernel_size=3, padding=1),
+            nn.Conv2d(self.in_channels, self.out_channels, kernel_size=3, padding=1),
             nn.GroupNorm(num_groups=32, num_channels=self.out_channels, eps=1e-5),
         )
-
         self.emb_layers = nn.Sequential(
             nn.SiLU(),
             nn.Linear(
@@ -67,47 +66,50 @@ class ResidualBlock(nn.Module):
         :return: an [N x C x ...] Tensor of outputs.
         """
         h = self.in_layers(x)
-        emb_out = self.emb_layers(emb)
-        assert len(emb_out.shape) == len(h.shape)
+
+        emb_out = self.emb_layers(emb)[:,:,None,None]
         if self.use_scale_shift_norm:
             scale, shift = torch.chunk(emb_out, 2, dim=1)
             h = h * (1 + scale) + shift
         else:
             h = h + emb_out
         h = self.out_layers(h)
+
         return x + h
 
 class UpResidualBlock(ResidualBlock):
     def forward(self, x, emb):
-        h = self.in_layers[:1](x)
+        h = self.in_layers[0](x) # First group norm
         h = F.interpolate(h, scale_factor=2, mode="nearest")
-        x = F.interpolate(x, scale_factor=2, mode="nearest")
-        h = self.in_layers[1:](x)
-        emb_out = self.emb_layers(emb)
-        assert len(emb_out.shape) == len(h.shape)
+        h = self.in_layers[1:](h)
+
+        emb_out = self.emb_layers(emb)[:,:,None,None]
         if self.use_scale_shift_norm:
             scale, shift = torch.chunk(emb_out, 2, dim=1)
             h = h * (1 + scale) + shift
         else:
             h = h + emb_out
         h = self.out_layers(h)
+
+        x = F.interpolate(x, scale_factor=2, mode="nearest")
         return x + h
 
 
 class DownResidualBlock(ResidualBlock):
     def forward(self, x, emb):
-        h = self.in_layers[:1](x)
+        h = self.in_layers[0](x) # First group norm
         h = F.avg_pool2d(h, kernel_size=(1, 2, 2))
-        x = F.avg_pool2d(x, kernel_size=(1, 2, 2))
-        h = self.in_layers[1:](x)
-        emb_out = self.emb_layers(emb)
-        assert len(emb_out.shape) == len(h.shape)
+        h = self.in_layers[1:](h)
+
+        emb_out = self.emb_layers(emb)[:,:,None,None]
         if self.use_scale_shift_norm:
             scale, shift = torch.chunk(emb_out, 2, dim=1)
             h = h * (1 + scale) + shift
         else:
             h = h + emb_out
         h = self.out_layers(h)
+
+        x = F.avg_pool2d(x, kernel_size=(1, 2, 2))
         return x + h
 
 
@@ -121,7 +123,7 @@ class QKVAttention(QKVMultiheadAttention):
         scale = 1 / math.sqrt(math.sqrt(attn_ch))
         qkv = qkv.view(bs, n_ctx, self.n_heads, -1)
         q, k, v = torch.split(qkv, attn_ch, dim=-1)
-        if encoder_kv is not None:
+        if encoder_kv:
             assert encoder_kv.shape[1] == self.n_heads * attn_ch * 2
             ek, ev = encoder_kv.reshape(bs * self.n_heads, attn_ch * 2, -1).split(attn_ch, dim=1)
             k = torch.cat([ek, k], dim=-1)
@@ -141,7 +143,6 @@ class AttentionBlock(nn.Module):
             channels,
             num_heads=1,
             num_head_channels=-1,
-            use_checkpoint=False,
             encoder_channels=None,
     ):
         super().__init__()
@@ -165,3 +166,106 @@ class AttentionBlock(nn.Module):
         y = self.attention(qkv, encoder_out)
         y = self.proj_out(y)
         return x + y.reshape(x.shape)
+
+class UNetModel(nn.Module):
+    """
+    The full UNet model with attention and timestep embedding.
+
+    :param in_channels: channels in the input Tensor.
+    :param model_channels: base channel count for the model.
+    :param out_channels: channels in the output Tensor.
+    :param num_res_blocks: number of residual blocks per downsample.
+    :param attention_resolutions: a collection of downsample rates at which
+        attention will take place. May be a set, list, or tuple.
+        For example, if this contains 4, then at 4x downsampling, attention
+        will be used.
+    :param dropout: the dropout probability.
+    :param channel_mult: channel multiplier for each level of the UNet.
+    :param conv_resample: if True, use learned convolutions for upsampling and
+        downsampling.
+    :param dims: determines if the signal is 1D, 2D, or 3D.
+    :param num_classes: if specified (as an int), then this model will be
+        class-conditional with `num_classes` classes.
+    :param use_checkpoint: use gradient checkpointing to reduce memory usage.
+    :param num_heads: the number of attention heads in each attention layer.
+    :param num_heads_channels: if specified, ignore num_heads and instead use
+                               a fixed channel width per attention head.
+    :param num_heads_upsample: works with num_heads to set a different number
+                               of heads for upsampling. Deprecated.
+    :param use_scale_shift_norm: use a FiLM-like conditioning mechanism.
+    :param resblock_updown: use residual blocks for up/downsampling.
+    """
+
+    def __init__(
+        self,
+        in_channels,
+        model_channels,
+        out_channels,
+        num_res_blocks,
+        attention_resolutions,
+        dropout=0,
+        channel_mult=(1, 2, 4, 8),
+        conv_resample=True,
+        num_classes=None,
+        use_checkpoint=False,
+        num_heads=1,
+        num_head_channels=-1,
+        num_heads_upsample=-1,
+        use_scale_shift_norm=False,
+        resblock_updown=False,
+        encoder_channels=None,
+    ):
+        super().__init__()
+
+        if num_heads_upsample == -1:
+            num_heads_upsample = num_heads
+
+        self.in_channels = in_channels
+        self.model_channels = model_channels
+        self.out_channels = out_channels
+        self.num_res_blocks = num_res_blocks
+        self.attention_resolutions = attention_resolutions
+        self.dropout = dropout
+        self.channel_mult = channel_mult
+        self.conv_resample = conv_resample
+        self.num_classes = num_classes
+        self.use_checkpoint = use_checkpoint
+        self.dtype = torch.float32
+        self.num_heads = num_heads
+        self.num_head_channels = num_head_channels
+        self.num_heads_upsample = num_heads_upsample
+
+        time_embed_dim = model_channels * 4
+        self.time_embed = nn.Sequential(
+            nn.Linear(model_channels, time_embed_dim),
+            nn.SiLU(),
+            nn.Linear(time_embed_dim, time_embed_dim),
+        )
+
+        next_channels = current_channels = int(channel_mult[0] * model_channels)
+        self.input_blocks = nn.ModuleList(
+            [nn.Conv2d(current_channels, next_channels, kernel_size=3, padding=1)]
+        )
+
+        input_block_channels = [current_channels]
+        ds = 1 #???
+        for level, mult in enumerate(channel_mult):
+            current_channels, next_channels = next_channels, int(mult * model_channels)
+            for _ in range(num_res_blocks):
+                layers = [
+                    ResidualBlock(
+                        current_channels,
+                        time_embed_dim,
+                        dropout=dropout,
+                        out_channels = next_channels,
+                        use_scale_shift_norm = use_scale_shift_norm
+                    )
+                ]
+
+
+
+
+
+
+
+
